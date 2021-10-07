3f02165d0f7ef02c854af3a285bbaf6545b8a988
fresaletlora <mikhaelfresa@gmail.com>
2021-10-07 17:50:04 +0700

    coba ganti route api.php

--- routes/api.php
+++ routes/api.php
@@ -32,28 +32,28 @@ Route::post('loginan', [LoginController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function(){
 
-    Route::middleware(['admin'])->group(function () {
-        Route::get('/admin_check', function () {
-            return response()->json([
-                'status' => 200
-            ], 200);
-        });
+    // Route::middleware(['admin'])->group(function () {
+    //     Route::get('/admin_check', function () {
+    //         return response()->json([
+    //             'status' => 200
+    //         ], 200);
+    //     });
 
-        Route::post('/add-hospital-data', [HospitalDataController::class, 'create']);
-        Route::post('/add-doctor-category', [DoctorCategoryController::class, 'create']);
-        Route::post('/add-doctor', [DataDoctorController::class, 'create']);
+    //     Route::post('/add-hospital-data', [HospitalDataController::class, 'create']);
+    //     Route::post('/add-doctor-category', [DoctorCategoryController::class, 'create']);
+    //     Route::post('/add-doctor', [DataDoctorController::class, 'create']);
 
-        Route::post('/shop/delete/{id}', [ShopController::class, 'delete']);
-        Route::post('/shop/add', [ShopController::class, 'store']);
+    //     Route::post('/shop/delete/{id}', [ShopController::class, 'delete']);
+    //     Route::post('/shop/add', [ShopController::class, 'store']);
 
-        Route::post('/article/create', [HealthInformationController::class, 'post']);
-        Route::post('/article/draft', [HealthInformationController::class, 'draft']);
-        Route::post('/article/delete', [HealthInformationController::class, 'delete']);
-        Route::get('/article/label', [HealthInformationController::class, 'showLabel']);
-        Route::get('/article/show', [HealthInformationController::class, 'showAllArticle']);
+    //     Route::post('/article/create', [HealthInformationController::class, 'post']);
+    //     Route::post('/article/draft', [HealthInformationController::class, 'draft']);
+    //     Route::post('/article/delete', [HealthInformationController::class, 'delete']);
+    //     Route::get('/article/label', [HealthInformationController::class, 'showLabel']);
+    //     Route::get('/article/show', [HealthInformationController::class, 'showAllArticle']);
 
-        Route::get('/admin/profile', [AdminController::class, 'getProfile']);
-    });
+    //     Route::get('/admin/profile', [AdminController::class, 'getProfile']);
+    // });
 
     Route::get('user', [UserController::class, 'fetch']);
     Route::post('user', [UserController::class, 'updateProfile']);
